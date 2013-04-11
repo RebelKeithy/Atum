ef969870b94c3075acc57433e49dfa9804645ec7
logomaster256 <marko.lazaric@gmail.com>
2013-04-11 18:40:03 +0200

    Fixed bonemeal not being used up

--- common/rebelkeithy/mods/atum/BonemealEventListener.java
+++ common/rebelkeithy/mods/atum/BonemealEventListener.java
@@ -2,6 +2,7 @@ package rebelkeithy.mods.atum;
 
 import java.util.Random;
 
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.player.BonemealEvent;
 import rebelkeithy.mods.atum.blocks.BlockAtumSapling;
@@ -15,14 +16,25 @@ public class BonemealEventListener
 		if(event.world.isRemote)
 			return true;
 		
+		boolean shouldDecrease = false;
 		int id = event.world.getBlockId(event.X, event.Y, event.Z);
 		if(id == Atum.atumPalmSapling.blockID)
 		{
 			((BlockAtumSapling)(Atum.atumPalmSapling)).growTree(event.world, event.X, event.Y, event.Z, new Random());
+			shouldDecrease = true;
 		}
 		if(id == Atum.atumFlax.blockID)
 		{
 			((BlockFlax)(Atum.atumFlax)).fertilize(event.world, event.X, event.Y, event.Z);
+			shouldDecrease = true;
+		}
+		if(shouldDecrease && !event.entityPlayer.capabilities.isCreativeMode)
+		{
+		    ItemStack itemstack = event.entityPlayer.getCurrentEquippedItem();
+            
+            --itemstack.stackSize;
+            
+            event.entityPlayer.setCurrentItemOrArmor(0, itemstack);
 		}
 		return true;
 	}
